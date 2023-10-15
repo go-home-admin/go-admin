f999c589d2a0da1a763d6e44976add6a8fe587de
jinzhu <1992053348@qq.com>
2023-10-15 12:19:28 +0800

    修改默认引导版本

--- app/providers/response_provider.go
+++ app/providers/response_provider.go
@@ -3,6 +3,7 @@ package providers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-home-admin/home/app/http"
+	"github.com/sirupsen/logrus"
 	http2 "net/http"
 	"strings"
 )
@@ -12,6 +13,9 @@ import (
 type Response struct{}
 
 func (r *Response) Init() {
+	// NewContext 最好在中间件已经赋值以下两个参数
+	// ginCtx.Set("user", nil)
+	// ginCtx.Set("user_id", nil)
 	http.NewContext = func(ctx *gin.Context) http.Context {
 		return &Ctx{
 			Context: ctx,
@@ -21,21 +25,21 @@ func (r *Response) Init() {
 
 type Ctx struct {
 	*gin.Context
-	UserID   uint64
+
 	UserInfo interface{}
 }
 
 func (receiver Ctx) Success(data interface{}) {
 	receiver.JSON(http2.StatusOK, map[string]interface{}{
 		"data": data,
-		"code": 200,
+		"code": 0,
 		"msg":  "",
 	})
 }
 
 func (receiver Ctx) Fail(err error) {
 	receiver.JSON(http2.StatusOK, map[string]interface{}{
-		"code": 502,
+		"code": 1,
 		"msg":  err.Error(),
 	})
 }
@@ -47,8 +51,22 @@ func (receiver Ctx) Gin() *gin.Context {
 func (receiver Ctx) User() interface{} {
 	return receiver.UserInfo
 }
+
 func (receiver Ctx) Id() uint64 {
-	return receiver.UserID
+	u, ok := receiver.Context.Get(http.UserIdKey)
+	if !ok {
+		logrus.Fatal("id 不存在, todo Context.Set(UserIdKey, Uid)")
+		return 0
+	}
+	return u.(uint64)
+}
+
+func (receiver Ctx) IdStr() string {
+	u, ok := receiver.Context.Get(http.UserIdKey)
+	if !ok {
+		return ""
+	}
+	return u.(string)
 }
 
 func (receiver Ctx) Token() string {
